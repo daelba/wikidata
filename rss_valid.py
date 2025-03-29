3738ad3d19b907c8921321028b8014e90e036ac2
Daniel Baránek <daniel.baranek@gmail.com>
2025-03-29 22:15:48 +0100

    specify QID of http error

--- rss_valid.py
+++ rss_valid.py
@@ -15,27 +15,37 @@ def process_url(res):
     response = check_response(url)
     if response < 400:
         print(response)
-        invalid_entry = f'-STATEMENT|{statement} /* RSS not reachable ({response}) + constraint violation (https://www.wikidata.org/wiki/Property:P1019#P2302) */'
+        #invalid_entry = f'-STATEMENT|{statement} /* RSS not reachable ({response}) + constraint violation (https://www.wikidata.org/wiki/Property:P1019#P2302) */'
+        invalid_entry = f'{statement}'
         return f"{item}: The URL {url} is an invalid RSS feed.", invalid_entry
 
-query = '''SELECT ?stat ?rss WHERE { ?item wdt:P31 wd:Q5; p:P1019 ?stat. ?stat ps:P1019 ?rss }'''
+errors = {
+    "403": "Q1138586",
+    "404": "Q1193907"
+}
+#query = '''SELECT ?stat ?rss WHERE { ?item wdt:P31 wd:Q5; p:P1019 ?stat. ?stat ps:P1019 ?rss }'''
 
-#query = '''SELECT ?stat ?rss WHERE { ?item p:P1019 ?stat. ?stat ps:P1019 ?rss. MINUS {?item wdt:P31 wd:Q5 } }'''
+query = '''SELECT ?stat ?rss WHERE {
+    ?item p:P1019 ?stat.
+    MINUS {?item wdt:P31 wd:Q5 }
+    ?stat ps:P1019 ?rss; wikibase:rank wikibase:NormalRank.
+}'''
 result = get_bigData(endpoint_wd, query, offset=0, max=100000)
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-    futures = {executor.submit(process_url, res): res for res in result}
+    futures = {executor.submit(check_response, res["rss"]["value"]): res for res in result}
     valid_count = 0
     total_count = len(result)
 
     for i, future in enumerate(concurrent.futures.as_completed(futures), start=1):
         res = futures[future]
         try:
-            message, invalid_entry = future.result()
-            print(f"{i}/{total_count} {message}") #, end="\r")
-            if invalid_entry:
-                print(invalid_entry)
+            response = future.result()
+            #print(response)
+            print(f"{i}/{total_count}", end="\r")
+            if response and response > 399:
+                statement = res["stat"]["value"].split("/")[-1].replace("-", "$", 1)
                 with open(f"rss_invalid.txt", "a") as file:
-                    file.write(invalid_entry + "\n")
+                    file.write(f"{statement}|deprecated|{errors[str(response)]}" + "\n")
         except Exception as e:
             print(f"Error processing URL {res['rss']['value']}: {e}")#, end="\r")
