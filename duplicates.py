176539a24f146148b53d444062549bce0841fc02
Daniel Baránek <daniel.baranek@gmail.com>
2025-03-30 21:37:51 +0200

    check deathdate

--- duplicates.py
+++ duplicates.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime, timedelta
 from difflib import SequenceMatcher
+from endpoints import *
 
 def get_sparql_query(date):
     return f"""
@@ -27,7 +28,11 @@ def fetch_people_born_on_date(date):
 def check_similarity(name1, name2):
     return SequenceMatcher(None, name1, name2).ratio() * 100
 
-year = 1880
+def get_q_values(qid, prop):
+    query = f"SELECT ?value WHERE {{ wd:{qid} p:{prop} ?stat. ?stat ps:{prop} ?value. MINUS {{ ?stat wikibase:rank wikibase:DeprecatedRank }} }}"
+    return sparql(endpoint_wd, query)["results"]["bindings"]
+
+year = 1890
 start_date = datetime(year, 1, 1)
 end_date = datetime(year + 9, 12, 31)
 current_date = start_date
@@ -42,6 +47,9 @@ while current_date <= end_date:
             name2 = people[j]['personLabel']['value']
             similarity = check_similarity(name1, name2)
             if similarity > 95:
-                print(f"Date: {date_str}, Item IDs: [{people[i]['person']['value']}, {people[j]['person']['value']}], Similarity: {similarity}%")
+                died1 = get_q_values(people[i]['person']['value'].split("/")[-1], 'P570')
+                died2 = get_q_values(people[j]['person']['value'].split("/")[-1], 'P570')
+                if not (died1 and died2) or any(d1 in died2 for d1 in died1):
+                    print(f"Date: {date_str}, Item IDs: [{people[i]['person']['value']} (died {died1[0]['value']['value'].split('T')[0] if died1 else '?'}), {people[j]['person']['value']} (died {died2[0]['value']['value'].split('T')[0] if died2 else '?'})], Similarity: {similarity}%")
     
     current_date += timedelta(days=1)
