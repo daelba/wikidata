0c4383da8fba0a5a8b1758186cd18d6a6ab16ecf
Daniel Baránek <daniel.baranek@gmail.com>
2025-04-01 18:03:42 +0200

    get_q_dates

--- duplicates.py
+++ duplicates.py
@@ -32,7 +32,17 @@ def get_q_values(qid, prop):
     query = f"SELECT ?value WHERE {{ wd:{qid} p:{prop} ?stat. ?stat ps:{prop} ?value. MINUS {{ ?stat wikibase:rank wikibase:DeprecatedRank }} }}"
     return sparql(endpoint_wd, query)["results"]["bindings"]
 
-year = 1890
+def get_q_date(qid, prop):
+    query = f"""SELECT ?value WHERE {{ wd:{qid} p:{prop} ?statement.
+ MINUS {{ ?stat wikibase:rank wikibase:DeprecatedRank }}
+ ?statement psv:{prop} ?timenode.
+ ?timenode wikibase:timeValue ?value; wikibase:timePrecision ?precision.
+ FILTER (?precision > 8).
+}}
+"""
+    return [ val["value"]["value"].split("T")[0] for val in sparql(endpoint_wd, query)["results"]["bindings"] ]
+
+year = 1920
 start_date = datetime(year, 1, 1)
 end_date = datetime(year + 9, 12, 31)
 current_date = start_date
@@ -47,9 +57,16 @@ while current_date <= end_date:
             name2 = people[j]['personLabel']['value']
             similarity = check_similarity(name1, name2)
             if similarity > 95:
-                died1 = get_q_values(people[i]['person']['value'].split("/")[-1], 'P570')
-                died2 = get_q_values(people[j]['person']['value'].split("/")[-1], 'P570')
-                if not (died1 and died2) or any(d1 in died2 for d1 in died1):
-                    print(f"Date: {date_str}, Item IDs: [{people[i]['person']['value']} (died {died1[0]['value']['value'].split('T')[0] if died1 else '?'}), {people[j]['person']['value']} (died {died2[0]['value']['value'].split('T')[0] if died2 else '?'})], Similarity: {similarity}%")
+                qid1 = people[i]['person']['value'].split("/")[-1]
+                qid2 = people[j]['person']['value'].split("/")[-1]
+                died1 = get_q_date(qid1, 'P570')
+                died2 = get_q_date(qid2, 'P570')
+                died_same = [ d1 for d1 in died1 if any(d2 == d1 for d2 in died2) ]
+                if died_same:
+                    with open("wd_merge.txt", "a") as file:
+                        file.write(f"MERGE|{qid1}|{qid2}\n")
+                # elif not (died1 and died2) or any(d1 in died2 for d1 in died1):
+                else:
+                    print(f"Date: {date_str}, Item IDs: [{people[i]['person']['value']} (died {died1 if died1 else '?'}), {people[j]['person']['value']} (died {died2 if died2 else '?'})], Similarity: {similarity}%")
     
     current_date += timedelta(days=1)
